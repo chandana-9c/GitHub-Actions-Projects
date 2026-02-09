c6fba2d3138d6f317e00d8f0d953c4374b70b365
Chandana <nayakchandanac@gmail.com>
2026-02-09 14:36:56 +0530

    Update index.js - render

--- github-cicd-webapp/index.js
+++ github-cicd-webapp/index.js
@@ -13,12 +13,13 @@ app.listen(port, () => console.log(`Server running on port ${port}`));
 const http = require('http');
 
 const hostname = '0.0.0.0';
-const port = 3000;
+//const port = 3000;
+const port = process.env.PORT; //render
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('<h1>Hello, Github CI/CD World!</h1>');
+  res.end('Hello, Github CI/CD World!');
 
 });
 
