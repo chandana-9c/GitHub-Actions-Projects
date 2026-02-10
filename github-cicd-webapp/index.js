006012338faa5615bef8e5598c804a0157cd001b
Chandana <nayakchandanac@gmail.com>
2026-02-10 19:08:51 +0530

    Update index.js - test Deploy after CI check pass

--- github-cicd-webapp/index.js
+++ github-cicd-webapp/index.js
@@ -5,7 +5,6 @@ const port = process.env.PORT || 3000;
 app.get('/', (req, res) => res.send('Hello, GitHub CI/CD World!'));
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
-
 */
 // index.js
 const http = require('http');
